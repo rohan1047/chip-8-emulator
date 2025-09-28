8514d3c4b86785fc20315f08106c1066d606e700
Rohan <rohansriram47@gmail.com>
2025-09-28 22:40:17 +0530

    Add Header File

---
+++ src/chip8.h
@@ -0,0 +1,33 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
+#include <array>
+#include <cstdint>
+
+class Chip8 {
+public:
+    Chip8();
+    void initialize();
+    void loadROM(const char* filename);
+    void cycle();
+
+    uint16_t pc;
+    uint16_t opcode;
+    uint16_t I;
+    uint16_t sp;
+
+    std::array<uint8_t, 64 * 32> gfx;
+    bool drawFlag;
+
+    std::array<uint8_t, 4096> memory;
+    std::array<uint8_t, 16> V;
+    std::array<uint16_t, 16> stack;
+
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+
+    uint8_t key[16];
+    size_t romSize;
+};
+
+#endif
